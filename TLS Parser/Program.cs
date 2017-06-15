1ffd7ee657fbdcda23785682211fac9c68d78795
EeeasyBreezy <ag1011@yandex.ru>
2017-06-15 16:10:55 +0300

    Поэтапный вывод действий

--- TLS Parser/Program.cs
+++ TLS Parser/Program.cs
@@ -11,26 +11,66 @@ namespace TLS_Parser
     class Program
     {
         static void Main(string[] args)
-        {
+       {
+            #region INIT
+            Console.Write("Creating client.......");
             WebScraperClient client = new WebScraperClient(Encoding.GetEncoding("UTF-8"));
             string htmlResponse = client.NavigateGet("http://webscraper.io/test-sites/e-commerce/allinone");
+            Console.WriteLine("OK");
+            #endregion
+
+            #region TOP PHONES
+            Console.Write("Parsing top phones.......");
             htmlResponse = client.NavigateGet("http://webscraper.io/test-sites/e-commerce/allinone/phones");
             List<Item> topPhones = WebScrapperParser.Parse(htmlResponse);
+            Console.WriteLine("OK");
+            #endregion
+
+            #region TOP COMPUTERS
+            Console.Write("Parsing top computers.......");
             htmlResponse = client.NavigateGet("http://webscraper.io/test-sites/e-commerce/allinone/computers");
             List<Item> topComputers = WebScrapperParser.Parse(htmlResponse);
+            Console.WriteLine("OK");
+            #endregion
+
+            #region LAPTOPS
+            Console.Write("Parsing laptops.......");
             htmlResponse = client.NavigateGet("http://webscraper.io/test-sites/e-commerce/allinone/computers/laptops");
             List<Item> laptops = WebScrapperParser.Parse(htmlResponse);
+            Console.WriteLine("OK");
+            #endregion
+
+            #region TABLETS
+            Console.Write("Parsing tablets..........");
             htmlResponse = client.NavigateGet("http://webscraper.io/test-sites/e-commerce/allinone/computers/tablets");
             List<Item> tablets = WebScrapperParser.Parse(htmlResponse);
+            Console.WriteLine("OK");
+            #endregion
+
+            #region TOUCH PHONES
+            Console.Write("Parsing touch phones.......");
             htmlResponse = client.NavigateGet("http://webscraper.io/test-sites/e-commerce/allinone/phones/touch");
             List<Item> touchPhones = WebScrapperParser.Parse(htmlResponse);
+            Console.WriteLine("OK");
+            #endregion
+
+            #region SERIALIZE
+            Console.Write("Serializing data to json.......");
             WebScraperData data = new WebScraperData(topPhones, topComputers, laptops,
                 tablets, touchPhones);
             DataContractJsonSerializer serializer =
                 new DataContractJsonSerializer(typeof(WebScraperData));
             StreamWriter stream = new StreamWriter("items.json");
             serializer.WriteObject(stream.BaseStream, data);
-            Console.WriteLine("navigation finished");
+            #endregion
+
+            #region FINISH
+            Console.WriteLine("OK");
+            Console.WriteLine("Serialization completed to file items.json");
+            Console.WriteLine("Parsing finished");
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+            #endregion
         }
     }
 }
